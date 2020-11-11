3d4a704f8f1737f58423542a033a09cb543b743d
bvezilic <banex93@gmail.com>
2020-11-11 17:19:59 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import underconstruction from './under_construction.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={underconstruction} className="App-underconstruction" alt="underconstruction" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
