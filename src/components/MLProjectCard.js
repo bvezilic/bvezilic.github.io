0d9352b00b807ecf691068d4ccaa0dd99878ebef
bvezilic <banex93@gmail.com>
2021-04-14 11:13:26 +0200

    Added minWidth to display card image

--- src/components/MLProjectCard.js
+++ src/components/MLProjectCard.js
@@ -11,35 +11,41 @@ import {
 
 
 const useStyles = makeStyles({
-    root: {
-      // display: 'flex',
-      maxWidth: 345,
-      margin: 6,
-    },
-    media: {
-      height: 150,
-      maxWidth: 150,
-    },
-  });
+  root: {
+    display: 'flex',
+    // maxWidth: 345,
+    // margin: 6,
+  },
+  media: {
+    height: 150,
+    minWidth: 150,
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+});
   
 export default function MLProjectCard(props) {
   const classes = useStyles();
 
   return (
     <Card variant="outlined" className={classes.root}>
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="h2">{props.name}</Typography>
-        <Typography color="textSecondary" component="p">{props.description}</Typography>
-      </CardContent>
-      <CardActions>
-        <Button href={props.repo} startIcon={<GitHubIcon />}>Github</Button>
-      </CardActions>
       <CardMedia
         className={classes.media}
         image={props.image}
         title="bilosta"
         alt="pic"
       />
+      <div className={classes.details}>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">{props.name}</Typography>
+          <Typography color="textSecondary" component="p">{props.description}</Typography>
+        </CardContent>
+        <CardActions>
+          <Button href={props.repo} startIcon={<GitHubIcon />}>Github</Button>
+        </CardActions>
+      </div>
     </Card>
   )
 }
